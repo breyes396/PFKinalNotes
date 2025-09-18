5cddc45f9d4d71091102b486df69ca9fa29db0d5
AADLCC <adelacruz-2021326@kinal.edu.gt>
2025-09-18 16:01:29 -0600

    s2: T022

---
+++ src/main/resources/data.sql
@@ -0,0 +1,33 @@
+insert ignore into Usuarios(correo, password , role)
+values ("wilsonDiaz@gmail.com","12345678","ESTUDIANTE");
+insert ignore into Usuarios(correo, password , role)
+values ("jeffersonDiaz@gmail.com","12345678","PROFESOR");
+insert ignore into Usuarios(correo, password , role)
+values ("matiasITcotoya@gmail.com","12345678","ADMINISTRADOR");
+insert ignore into Usuarios(correo, password , role)
+values ("saulSatbaja@gmail.com","12345678","ESTUDIANTE");
+insert ignore into Usuarios(correo, password , role)
+values ("wilsonMorales@gmail.com","12345678","PROFESOR");
+insert ignore into Usuarios(correo, password , role)
+values ("wilsonSosa@gmail.com","12345678","ADMINISTRADOR");
+insert ignore into Usuarios(correo, password , role)
+values ("wilsonDiaz@gmail.com","12345678","ESTUDIANTE");
+insert ignore into Usuarios(correo, password , role)
+values ("angelTucubal@gmail.com","12345678","PROFESOR");
+insert ignore into Usuarios(correo, password , role)
+values ("BillyDiaz@gmail.com","12345678","ADMINISTRADOR");
+insert ignore into Usuarios(correo, password , role)
+values ("wilsonYaxon@gmail.com","12345678","ESTUDIANTE");
+insert ignore into Usuarios(correo, password , role)
+values ("javierDiaz@gmail.com","12345678","PROFESOR");
+insert ignore into Usuarios(correo, password , role)
+values ("saulSipack@gmail.com","12345678","ADMINISTRADOR");
+insert ignore into Usuarios(correo, password , role)
+values ("matiasTuvubal@gmail.com","12345678","ESTUDIANTE");
+insert ignore into Usuarios(correo, password , role)
+values ("ricardoLucero@gmail.com","12345678","PROFESOR");
+insert ignore into Usuarios(correo, password , role)
+values ("BillyYaxon@gmail.com","12345678","ADMINISTRADOR");
+
+
+
