44b00ca0f3b5843dc6e4f50cf5879a30ebbd400c
wflorian <wflorian-2024392@kinal.edu.gt>
2025-09-26 16:50:18 -0600

    T051, T052, T054, T059, T060 completadas

--- src/main/java/org/binaryminds/kinalnotes/dominio/service/NotaService.java
+++ src/main/java/org/binaryminds/kinalnotes/dominio/service/NotaService.java
@@ -4,6 +4,7 @@ import org.binaryminds.kinalnotes.dominio.dto.NotaDto;
 import org.binaryminds.kinalnotes.dominio.dto.ModNotaDto;
 import org.binaryminds.kinalnotes.dominio.repository.NotaRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -15,30 +16,37 @@ public class NotaService {
         this.notaRepository = notaRepository;
     }
 
+    @Transactional(readOnly = true)
     public List<NotaDto> obtenerTodo(){
         return this.notaRepository.obtenerTodo();
     }
 
+    @Transactional(readOnly = true)
     public NotaDto obtenerNotaPorCodigo(Long codigo){
         return this.notaRepository.obtenerNotaPorCodigo(codigo);
     }
 
+    @Transactional
     public NotaDto guardarNota(NotaDto notaDto){
         return this.notaRepository.guardarNota(notaDto);
     }
 
+    @Transactional
     public NotaDto actualizarNota(Long codigo, ModNotaDto modNotaDto){
         return this.notaRepository.actualizarNota(codigo, modNotaDto);
     }
 
+    @Transactional
     public void eliminarNota(Long codigo){
         this.notaRepository.eliminarNota(codigo);
     }
 
+    @Transactional(readOnly = true)
     public NotaDto obtenerNotaPorCalificacion(Integer calificacion) {
         return this.notaRepository.obtenerNotaPorCalificacion(calificacion);
     }
 
+    @Transactional(readOnly = true)
     public NotaDto obtenerNotaPorEstudianteCursoDocente(Long estudiante, Long curso, Long docente){
         return this.notaRepository.obtenerNotaPorEstudianteCursoDocente(estudiante, curso, docente);
     }
